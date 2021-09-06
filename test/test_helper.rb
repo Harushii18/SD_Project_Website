132040145f51ac084405071d211b3c11447fa9b4
RXRider1451 <RXRider1451@Gmail.com>
2021-09-06 21:54:47 +0200

    Changed TravisCI to execute test with coveralls & removed wear!('rails'.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 require 'coveralls'
-Coveralls.wear!('rails')
+Coveralls.wear!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
