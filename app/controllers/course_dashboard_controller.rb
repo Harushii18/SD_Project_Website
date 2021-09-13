622e5875af5dc4062b14a799fa0fc781c7916034
Suraksha Motilal <surakshamotilal@gmail.com>
2021-09-13 16:58:18 +0200

    Added extraction from database for course dashboard's contents. Added extra validation that should there be no records, this is displayed.

--- app/controllers/course_dashboard_controller.rb
+++ app/controllers/course_dashboard_controller.rb
@@ -1,4 +1,7 @@
 class CourseDashboardController < ApplicationController
   def index
+    @courses = Course.all
+    @course_specialties = CourseSpecialty.all
+    @specialties = Specialty.all
   end
 end
