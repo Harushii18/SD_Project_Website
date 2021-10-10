74bb48db48308217e1ee0ebea849e5076e633067
exit <naickereston@gmail.com>
2021-10-10 22:03:47 +0200

    GS

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,146 +1,169 @@
-                                                                        class SchedulesController < ApplicationController
-  skip_forgery_protection
-  before_action :set_schedule, only: %i[ show edit update destroy ]
-  before_action :set_search
+class SchedulesController < ApplicationController
+skip_forgery_protection
+before_action :set_schedule, only: %i[ show edit update destroy ]
+before_action :set_search
 
 
-  # GET /schedules or /schedules.json
-  def index
-    @users = @q.result
-    @programmes = Programme.all
-    @courseSpecialties=CourseSpecialty.all
-    @programmCourses=ProgrammeCourse.all
-    @all_users = User.all
+# GET /schedules or /schedules.json
+def index
 
-    @specialties = Specialty.all
-    puts("look here")
-    @current_programme_id = 1
+@users = @q.result
+@programmes = Programme.all
+@courseSpecialties=CourseSpecialty.all
+@programmCourses=ProgrammeCourse.all
+@all_users = User.all
 
-    if (params[:programme_id])
-        @current_programme_id =  params[:programme_id]
-    #else
-    end
+@specialties = Specialty.all
+puts("look here")
 
 
-    @programme_courses = ProgrammeCourse.where(programme_id: @current_programme_id).select([:course_id])
-
-    @course_specialties = CourseSpecialty.where(course_id: @programme_courses).select([:specialty_id])
-
-    
-
-    @specialties = Specialty.where(id: @course_specialties)
+@FirstRec = Programme.first;
+@current_programme_id = @FirstRec.id;
 
 
-    @students = Student.filter_by_programme_id(@current_programme_id)
-    #@students = @students.find()
-    @schedules = Schedule.all
+if (params[:programme_id])
+@current_programme_id =  params[:programme_id];
 
-    @rotations = Rotation.filter_by_programme_id(@current_programme_id)
+end
+#to get the group ID
+@value;
+#The Recs in Student_groups based on @value
+@Student_Groups;
+#if the user clicks on groups modal box
+if(params[:Group_id])
+@value = params[:Group_id];
 
-    @numRotations = @rotations.length()
-  end
+end
 
-   def set_search
-     @q=User.ransack(params[:q])
-   end
+@Student_Groups = StudentGroup.where(group_id: @value);
 
+
+@programme_courses = ProgrammeCourse.where(programme_id: @current_programme_id).select([:course_id])
+
+@course_specialties = CourseSpecialty.where(course_id: @programme_courses).select([:specialty_id])
 
 
 
-  # GET /schedules/1 or /schedules/1.json
-  def show
-  end
+@specialties = Specialty.where(id: @course_specialties)
 
-  # GET /schedules/new
-  def new
-    @schedule = Schedule.new
-  end
 
-  # GET /schedules/1/edit
-  def edit
-  end
+@students = Student.filter_by_programme_id(@current_programme_id);
+#Get the Groups Ass with Prog
+@Groups = Group.where(programme_id: @current_programme_id);
 
-  # POST /schedules or /schedules.json
-  def create
-    @schedule = Schedule.new(schedule_params)
+# @Student_Groups = Student_groups.filter_by_group_id()
+#@students = @students.find()
+@schedules = Schedule.all
 
-    if(!Schedule.exists?(student_id:params[:student_id],specialty_id:params[:specialty_id],hospital_id:params[:hospital_id],week_no:params[:week_no],specialty_duration:params[:specialty_duration]   ))
-      respond_to do |format|
-          if @schedule.save
-            puts("kameron---------------------------Saved")
-             # format.html { redirect_to @schedule, notice: "Schedule was successfully created." }
-              format.json { render :show, status: :created, location: @schedule }
-          else
-            puts("kameron::::in schedules controller failed to save")
-            format.html { render :new, status: :unprocessable_entity }
-            format.json { render json: @schedule.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  end
+@rotations = Rotation.filter_by_programme_id(@current_programme_id)
 
-  # PATCH/PUT /schedules/1 or /schedules/1.json
-  def update
-    respond_to do |format|
-      if @schedule.update(schedule_params)
-        format.html { redirect_to @schedule, notice: "Schedule was successfully updated." }
-        format.json { render :show, status: :ok, location: @schedule }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @schedule.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+@numRotations = @rotations.length()
+end
 
-  def delete_with_student_id
-    #puts("In /schedules/delete_with_student_id,  params[:student_id] is : " +params[:student_id])
-    Schedule.delete_with_student_id( params[:student_id] ) #calls delete_with_student_id in model schedule.rb
-  end
+def set_search
+@q=User.ransack(params[:q])
+end
 
-  # DELETE /schedules/1 or /schedules/1.json
-  def destroy
-    @schedule.destroy
-    respond_to do |format|
-      format.html { redirect_to schedules_url, notice: "Schedule was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+def f
 
-  def create_schedule
-
-    if (params[:specialty_id]!='' && params[:hospital_id]!='')
-      if Schedule.exists?(student_id: params[:student_id], rotation_id: params[:rotation_id])
-        Schedule.update_with_form_data(params[:student_id], params[:rotation_id], params[:specialty_id], params[:hospital_id])
-      else
-        Schedule.create_with_form_data(params[:student_id], params[:rotation_id], params[:specialty_id], params[:hospital_id])
-      end
-    end
-
-  end
-
-  def update_schedule
-    if (params[:specialty_id]!='' && params[:hospital_id]!='')
-    Schedule.update_with_form_data(params[:student_id], params[:rotation_id], params[:specialty_id], params[:hospital_id])
-    end
-  end
+end
 
 
 
+# GET /schedules/1 or /schedules/1.json
+def show
+end
 
-  #def import # importing from csv file
-  #  User.import(params[:file]) #call User.import function in user.rb model file
-  #  redirect_to users_path, notice: "Users Added Successfully"
-  #end
+# GET /schedules/new
+def new
+@schedule = Schedule.new
+end
+
+# GET /schedules/1/edit
+def edit
+end
+
+# POST /schedules or /schedules.json
+def create
+@schedule = Schedule.new(schedule_params)
+
+if(!Schedule.exists?(student_id:params[:student_id],specialty_id:params[:specialty_id],hospital_id:params[:hospital_id],week_no:params[:week_no],specialty_duration:params[:specialty_duration]   ))
+respond_to do |format|
+if @schedule.save
+puts("kameron---------------------------Saved")
+# format.html { redirect_to @schedule, notice: "Schedule was successfully created." }
+format.json { render :show, status: :created, location: @schedule }
+else
+puts("kameron::::in schedules controller failed to save")
+format.html { render :new, status: :unprocessable_entity }
+format.json { render json: @schedule.errors, status: :unprocessable_entity }
+end
+end
+end
+end
+
+# PATCH/PUT /schedules/1 or /schedules/1.json
+def update
+respond_to do |format|
+if @schedule.update(schedule_params)
+format.html { redirect_to @schedule, notice: "Schedule was successfully updated." }
+format.json { render :show, status: :ok, location: @schedule }
+else
+format.html { render :edit, status: :unprocessable_entity }
+format.json { render json: @schedule.errors, status: :unprocessable_entity }
+end
+end
+end
+
+def delete_with_student_id
+#puts("In /schedules/delete_with_student_id,  params[:student_id] is : " +params[:student_id])
+Schedule.delete_with_student_id( params[:student_id] ) #calls delete_with_student_id in model schedule.rb
+end
+
+# DELETE /schedules/1 or /schedules/1.json
+def destroy
+@schedule.destroy
+respond_to do |format|
+format.html { redirect_to schedules_url, notice: "Schedule was successfully destroyed." }
+format.json { head :no_content }
+end
+end
+
+def create_schedule
+
+if (params[:specialty_id]!='' && params[:hospital_id]!='')
+if Schedule.exists?(student_id: params[:student_id], rotation_id: params[:rotation_id])
+Schedule.update_with_form_data(params[:student_id], params[:rotation_id], params[:specialty_id], params[:hospital_id])
+else
+Schedule.create_with_form_data(params[:student_id], params[:rotation_id], params[:specialty_id], params[:hospital_id])
+end
+end
+
+end
+
+def update_schedule
+if (params[:specialty_id]!='' && params[:hospital_id]!='')
+Schedule.update_with_form_data(params[:student_id], params[:rotation_id], params[:specialty_id], params[:hospital_id])
+end
+end
 
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_schedule
-      @schedule = Schedule.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def schedule_params
-      params.permit(:student_id, :specialty_id, :hospital_id , :week_no , :specialty_duration)
-    end
+
+#def import # importing from csv file
+#  User.import(params[:file]) #call User.import function in user.rb model file
+#  redirect_to users_path, notice: "Users Added Successfully"
+#end
+
+
+private
+# Use callbacks to share common setup or constraints between actions.
+def set_schedule
+@schedule = Schedule.find(params[:id])
+end
+
+# Only allow a list of trusted parameters through.
+def schedule_params
+params.permit(:student_id, :specialty_id, :hospital_id , :week_no , :specialty_duration)
+end
 end
