a01c09d28b0a4811c181c20f91fb2c89bb9c5439
Kiyolin Perumal <2115440@students.wits.ac.za>
2021-10-19 18:04:01 +0200

    test course dashboard

--- test/controllers/course_dashboard_controller_test.rb
+++ test/controllers/course_dashboard_controller_test.rb
@@ -1,9 +1,11 @@
 require "test_helper"
 
 class CourseDashboardControllerTest < ActionDispatch::IntegrationTest
-   test "the truth" do
-     assert true
-   end
+  setup do
+    get '/admins/sign_in'
+    sign_in admins(:admin_001)
+    post admin_session_url
+  end
 
   test "should get index" do
     get course_dashboard_index_url
