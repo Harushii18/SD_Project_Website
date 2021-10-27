182548d4f44c825e2cf34bb219599d1c21ffddfb
Kiyolin Perumal <2115440@students.wits.ac.za>
2021-10-27 21:12:40 +0200

    upload from csv works with programme_code

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -1,5 +1,5 @@
 class Programme < ApplicationRecord
-    has_many :students, :dependent => :delete_all
+    has_many :students, :dependent => :nullify
     has_many :groups, :dependent => :delete_all
     has_many :programme_courses, :dependent => :delete_all
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,31 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :email, uniqueness: true
 	validates :email, :user_FirstName,:user_Type, presence: true
-  has_one :student
+  has_one :student, :dependent => :delete
 
 	def self.import(file)
         CSV.foreach(file.path, headers: true) do |row|
           #format: User.new(:database_field_name => row["csv_column_header_name])
           @user = User.new(:email => row["email"], :user_FirstName => row["firstName"], :user_LastName => row["lastName"], :password => row["password"], :user_ContactNo => row["contactNo"], :user_Type => row["type"])
-          @user.save!
+
           
           #create a student with the same id as the user record
           puts(row["type"])
           if (row["type"] == "Student")
-            #c = Client.new(firm_id: id); c.save; 
-            #b = Beneficiary.new(account_id: id); b.save; b
-            student = Student.new(user_id: @user.id, :studentNumber => row["studentNumber"], :courseCodes => row["courseCodes"], :programme_id => row["programme_id"])
-            student.save
-          end
-        end
-      end
+            if @user.save!
+              @programme_temp = Programme.find_by(programme_code: row["programme_code"]).id
+              @student = Student.new(:studentNumber => row["studentNumber"], :courseCodes => row["courseCodes"], :programme_id => @programme_temp, :user_id => @user.id)
+          
+              @student.save!
+            end
+          else
+            @user.save!
+          end #if else
+
+
+
+        end #do
+      end #self.import
 
 end
 
